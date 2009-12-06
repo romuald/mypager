99a23743704a2eb422be5780fdd385c15301c55b
Romuald <romuald@strago.local>
2009-12-06 16:55:24 +0100

    tab => spaces

--- mpager.pl
+++ mpager.pl
@@ -20,10 +20,10 @@ my ($term_cols, $term_lines) = (0, 0);
 
 # Try to determine the screen size from module or stty
 eval {
-	require "Term/ReadKey.pm";
-	($term_cols, $term_lines) = Term::ReadKey::GetTerminalSize();
+    require "Term/ReadKey.pm";
+    ($term_cols, $term_lines) = Term::ReadKey::GetTerminalSize();
 } or eval {
-	($term_lines, $term_cols) = split /\s+/, `stty -F /dev/stderr size`;
+    ($term_lines, $term_cols) = split /\s+/, `stty -F /dev/stderr size`;
 };
 
 # Global print "buffer" scalar and filehandle
@@ -31,12 +31,12 @@ my $outhandle;
 my $outstring = "";
 
 open($outhandle, "+>", \$outstring)
-	or die("Can't create temporary buffer");
+    or die("Can't create temporary buffer");
 
 select($outhandle);
 END {
-	# If less was used, then outstring will be empty
-	print STDOUT $outstring;
+    # If less was used, then outstring will be empty
+    print STDOUT $outstring;
 }
 
 # First line with +---+-----+
@@ -49,31 +49,31 @@ my $magic;
 if ( $header =~ /^\+(?:-+\+)+$/ ) {
     my $start = 2;
     my @cols = split /\+/, $header;
-    
+
     for my $minus ( @cols[1..$#cols-1] ) {
         my $length = length($minus) - 2;
         next if $length < 0;
-        
+
         push @$columns, [$start, $length];
 
         $start += $length + 3;
     }
-	my $copy = $header;
-	chomp($copy);
-	
-	my $c = $copy =~ s{(?:\+-)(-+)(?:-(?=\+))}
-	{
-		"(" . '.{' . length($1) . "}) \Q|\E "
-	}gex;
+    my $copy = $header;
+    chomp($copy);
 
-	if ( $c > 0 ) {
-		$copy =~ s/\+$//;
-	}
+    my $c = $copy =~ s{(?:\+-)(-+)(?:-(?=\+))}
+    {
+        "(" . '.{' . length($1) . "}) \Q|\E "
+    }gex;
 
-	$magic = qr/$copy/s;
+    if ( $c > 0 ) {
+        $copy =~ s/\+$//;
+    }
+
+    $magic = qr/$copy/s;
 } else {
-	print <>;
-	exit;
+    print <>;
+    exit;
 }
 
 # XXX bold headers
@@ -83,22 +83,22 @@ for (1..2) {
 }
 
 sub colcol($) {
-	my $value = $_[0];
+    my $value = $_[0];
 
-	if ( $value =~ /[a-zA-KMO-TV-Z]/ ) {
-		return $value;
-	}
-	
-	if ( $value =~ $match_null ) {
-		return  $style_null . $value . $reset;
-	}
-	if ( $value =~ $match_int ) {
-		return $style_int . $value . $reset;
-	}
-	if ( $value =~ $match_date ) {
-		return $style_date . $value . $reset;
-	}
-	return $value;
+    if ( $value =~ /[a-zA-KMO-TV-Z]/ ) {
+        return $value;
+    }
+
+    if ( $value =~ $match_null ) {
+        return  $style_null . $value . $reset;
+    }
+    if ( $value =~ $match_int ) {
+        return $style_int . $value . $reset;
+    }
+    if ( $value =~ $match_date ) {
+        return $style_date . $value . $reset;
+    }
+    return $value;
 }
 
 sub max(@) { (sort @_)[-1] }
@@ -109,27 +109,27 @@ my $cur_lines = scalar(grep /\n/, $outstring);
 
 $/ = " | \n";
 while (my $line = <>) {
-	# since $/ has been changed, $line may contain multiple lines
-	if ( ! $useless ) {
-		$cur_lines += scalar(grep /\n/, $line);
-		$cur_cols = max($cur_cols, map {length} split( /\n/, $line) );
-		
-		if ( $cur_lines > $term_lines || $cur_cols - 1 > $term_cols) {
-			# Switch to less, and write current buffer
-			open($useless, '| less -R -S')
-				or die("Can't open less");
-			select($useless);
-						
-			print $useless $outstring;
-			close($outhandle);
-			$outstring = "";
-		}
-	}
-	
-	if ( my @values = $line =~ $magic ) {
-		print '| ', join( ' | ', map { colcol($_) } @values), $/;
-	} else {
-		print $line;
-	}
+    # since $/ has been changed, $line may contain multiple lines
+    if ( ! $useless ) {
+        $cur_lines += scalar(grep /\n/, $line);
+        $cur_cols = max($cur_cols, map {length} split( /\n/, $line) );
+
+        if ( $cur_lines > $term_lines || $cur_cols - 1 > $term_cols) {
+            # Switch to less, and write current buffer
+            open($useless, '| less -R -S')
+                or die("Can't open less");
+            select($useless);
+
+            print $useless $outstring;
+            close($outhandle);
+            $outstring = "";
+        }
+    }
+
+    if ( my @values = $line =~ $magic ) {
+        print '| ', join( ' | ', map { colcol($_) } @values), $/;
+    } else {
+        print $line;
+    }
 }
 
