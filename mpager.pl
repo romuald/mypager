e0533fbdeac3a7507d9854759678a0fedc8ee170
Romuald Brunet <romuald@gandi.net>
2009-12-08 19:01:43 +0100

    removed some unused code

--- mpager.pl
+++ mpager.pl
@@ -54,22 +54,6 @@ my $match_date = qr/^(?:[0-9]{4}-[0-9]{2}-[0-9]{2})|(?:[0-9]{2}:[0-9]{2}:[0-9]{2
 my $date = '\d{4}-\d{2}-\d{2}';
 my $time = '\d{2}:\d{2}:\d{2}';
 
-# Returns a "colored" version of a value
-sub coloredvalue($) {
-    my $value = $_[0];
-
-    if ( $value =~ $match_null ) {
-        return  $style_null . $value . $reset;
-    }
-    if ( $value =~ $match_int ) {
-        return $style_int . $value . $reset;
-    }
-    if ( $value =~ $match_date ) {
-        return $style_date . $value . $reset;
-    }
-    return $value;
-}
-
 # Quick max function :p
 sub max(@) { (sort @_)[-1] }
 
@@ -79,8 +63,8 @@ my $cur_lines = scalar(grep /\n/, $outstring);
 
 while (my $line = <>) {
     if ( ! $useless ) {
-        $cur_lines += $line =~ tr/\n/\n/;
-        $cur_cols = max($cur_cols, map {length} split( /\n/, $line) );
+        $cur_lines += 1;
+        $cur_cols = max($cur_cols, length($line));
 
         if ( $cur_lines > $term_lines || $cur_cols - 1 > $term_cols) {
             # Switch to less, and write current buffer
@@ -94,10 +78,6 @@ while (my $line = <>) {
         }
     }
     
-    #$line =~ s{\|\s([-. :0-9NUL]+)(?=\s\|)}{
-    #    "| " . coloredvalue($1) . "";
-    #}gex;
-
     $line =~ s/(\| +)(NULL +)(?=\|)/$1$style_null$2$reset/g;
     $line =~ s/(\| +)(-?\d+\.?\d* )(?=\|)/$1$style_int$2$reset/g;
     $line =~ s/(\| )((?:$date(?: $time)?|(?:$date )?$time) +)(?=\|)/$1$style_date$2$reset/g;
